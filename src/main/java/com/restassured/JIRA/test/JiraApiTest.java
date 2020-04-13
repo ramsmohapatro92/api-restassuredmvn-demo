7d1b122333c5acb72eb29f39599c5eaca58d068d
ramsmohapatro92 <ramsmohapatro92@gmail.com>
2020-04-14 01:59:55 +0530

    File Attcahment in JIRA through Rest Assured

--- src/main/java/com/restassured/JIRA/test/JiraApiTest.java
+++ src/main/java/com/restassured/JIRA/test/JiraApiTest.java
@@ -5,9 +5,12 @@ import org.testng.annotations.Test;
 import com.restassured.utility.ReusableMethods;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
 import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
+
+import java.io.File;
 
 public class JiraApiTest {
 
@@ -15,14 +18,19 @@ public class JiraApiTest {
 	static String sessionKey;
 	static String cookie_value;
 	static String defect_id;
+	static SessionFilter session_id; /*
+										 * Alternative to JSONPath where we are manually picking up the cookie values to
+										 * be validated through out our execution
+										 */
 
-	@Test
+	@Test(priority = 1)
 	public static void createSession() {
 		RestAssured.baseURI = "http://localhost:8090";
+		session_id = new SessionFilter();
 
 		String JiraCreateSessionResponse = given().header("Content-Type", "application/json").log().all()
-				.body(JiraPayLoad.JiraCreateSessionPayload()).when().post("/rest/auth/1/session").then().assertThat()
-				.statusCode(200).extract().response().asString();
+				.filter(session_id).body(JiraPayLoad.JiraCreateSessionPayload()).when().post("/rest/auth/1/session")
+				.then().assertThat().statusCode(200).extract().response().asString();
 
 		JsonPath js = ReusableMethods.jsonPathinitiator(JiraCreateSessionResponse);
 		sessionID = js.get("session.value");
@@ -32,13 +40,14 @@ public class JiraApiTest {
 
 	}
 
-	@Test(dependsOnMethods = "createSession", alwaysRun = true)
+	@Test(dependsOnMethods = "createSession", priority = 2)
 	public static void createIssue() {
 		RestAssured.baseURI = "http://localhost:8090";
 
-		String JiraCreateIssueResponse = given().header("Content-Type", "application/json")
-				.header("Cookie", cookie_value).body(JiraPayLoad.JiraCreateIssuePayload()).log().all().when()
-				.post("/rest/api/2/issue/").then().assertThat().statusCode(201).extract().asString();
+		String JiraCreateIssueResponse = given().header("Content-Type", "application/json").filter(session_id)
+				.body(JiraPayLoad.JiraCreateIssuePayload()).log().all().when().post("/rest/api/2/issue/").then()
+				.assertThat().statusCode(201).extract().asString();
+		/* header("Cookie", cookie_value) removed filter(session_id) used */
 
 		JsonPath js = ReusableMethods.jsonPathinitiator(JiraCreateIssueResponse);
 		defect_id = js.get("id");
@@ -46,25 +55,43 @@ public class JiraApiTest {
 
 	}
 
-	@Test(dependsOnMethods = "createIssue", alwaysRun = true)
+	@Test(dependsOnMethods = "createIssue", priority = 3)
 	public static void addComment() {
 		RestAssured.baseURI = "http://localhost:8090";
 
-		String JiraAddCommentResponse = given().header("Content-Type", "application/json")
-				.header("Cookie", cookie_value).body(JiraPayLoad.JiraAddCommentPayload()).log().all().when()
-				.post("http://localhost:8090/rest/api/2/issue/" + defect_id + "/comment").then().assertThat()
-				.statusCode(201).extract().asString();
-		
-		//Using PathParam function
-		
-//		String JiraAddCommentResponse1 = given().pathParam("{key}", cookie_value).header("Content-Type", "application/json")
-//				.header("Cookie", cookie_value).body(JiraPayLoad.JiraAddCommentPayload()).log().all().when()
-//				.post("http://localhost:8090/rest/api/2/issue/{key}/comment").then().assertThat()
-//				.statusCode(201).extract().asString();
+		String JiraAddCommentResponse = given().header("Content-Type", "application/json").filter(session_id)
+				.pathParam("key", defect_id).body(JiraPayLoad.JiraAddCommentPayload()).log().all().when()
+				.post("http://localhost:8090/rest/api/2/issue/{key}/comment").then().assertThat().statusCode(201)
+				.extract().asString();
 
+		/*
+		 * Using PathParam function 1. Manual concatenation to be avoided in POST():
+		 * "http://localhost:8090/rest/api/2/issue/" + defect_id + "/comment">>>
+		 * Replaced with pathParam("key", defect_id) 2. header("Cookie", cookie_value)
+		 * removed
+		 */
 		JsonPath js = ReusableMethods.jsonPathinitiator(JiraAddCommentResponse);
-		String commentResponse = js.get("Body.body");
+		String commentResponse = js.getString("Body.body");
 		System.out.println(commentResponse);
 	}
 
+	@Test(dependsOnMethods = "createIssue", priority = 4)
+	public static void addAttachment() {
+		RestAssured.baseURI = "http://localhost:8090";
+		File attachment_doc = new File("JIRAattachment");
+
+		// curl comment available in JIRA documentation:curl -D- -u admin:admin -X POST
+		// -H "X-Atlassian-Token: no-check" -F "file=@myfile.txt"
+		// http://myhost/rest/api/2/issue/TEST-123/attachments
+
+		String JiraAddAttachmentResponse = given().header("X-Atlassian-Token", "no-check").filter(session_id)
+				.pathParam("key", defect_id).header("Content-Type", "multipart/form-data")
+				.multiPart("file", attachment_doc).when().post("/rest/api/2/issue/{key}/attachments").then().log().all()
+				.assertThat().statusCode(200).extract().asString();
+
+		JsonPath js = ReusableMethods.jsonPathinitiator(JiraAddAttachmentResponse);
+		String fileAttached = js.getString("filename");
+		System.out.println(fileAttached);
+
+	}
 }
--- src/main/java/com/restassured/JIRA/test/JiraPayLoad.java
+++ src/main/java/com/restassured/JIRA/test/JiraPayLoad.java
@@ -1,38 +1,27 @@
 package com.restassured.JIRA.test;
 
 public class JiraPayLoad {
-	
+
 	public static String JiraCreateSessionPayload() {
-		
-		String createSessionPayload="{ \"username\": \"Admin92\", \"password\": \"Jira@123\" }";
+
+		String createSessionPayload = "{ \"username\": \"Admin92\", \"password\": \"Jira@123\" }";
 		return createSessionPayload;
 	}
-	
-	public static String JiraCreateIssuePayload(){
-		String createIssuePayload="{\r\n" + 
-				"    \"fields\": {\r\n" + 
-				"       \"project\":\r\n" + 
-				"       {\r\n" + 
-				"          \"key\": \"JIRARES\"\r\n" + 
-				"       },\r\n" + 
-				"       \"summary\": \"Issue Created to update comment for Rest Assured\",\r\n" + 
-				"       \"description\": \"Debit card UI issue practical from Rest Assured\",\r\n" + 
-				"       \"issuetype\": {\r\n" + 
-				"          \"name\": \"Bug\"\r\n" + 
-				"       }\r\n" + 
-				"   }\r\n" + 
-				"}";
+
+	public static String JiraCreateIssuePayload() {
+		String createIssuePayload = "{\r\n" + "    \"fields\": {\r\n" + "       \"project\":\r\n" + "       {\r\n"
+				+ "          \"key\": \"JIRARES\"\r\n" + "       },\r\n"
+				+ "       \"summary\": \"New CASATD:Issue Created to update comment and Attachment for Rest Assured\",\r\n"
+				+ "       \"description\": \"New CASATD Product: UI issue practical from Rest Assured\",\r\n"
+				+ "       \"issuetype\": {\r\n" + "          \"name\": \"Bug\"\r\n" + "       }\r\n" + "   }\r\n" + "}";
 		return createIssuePayload;
 	}
-	public static String JiraAddCommentPayload()
-	{
-		String addCommentPayload="{\r\n" + 
-				"    \"body\": \"Initial added comment by Ram from Rest Assured\",\r\n" + 
-				"    \"visibility\": {\r\n" + 
-				"        \"type\": \"role\",\r\n" + 
-				"        \"value\": \"Administrators\"\r\n" + 
-				"    }\r\n" + 
-				"}";
+
+	public static String JiraAddCommentPayload() {
+		String addCommentPayload = "{\r\n"
+				+ "    \"body\": \"Initial added comment by Ram from Rest Assured and Attachment added\",\r\n"
+				+ "    \"visibility\": {\r\n" + "        \"type\": \"role\",\r\n"
+				+ "        \"value\": \"Administrators\"\r\n" + "    }\r\n" + "}";
 		return addCommentPayload;
 	}
 }
